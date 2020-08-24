6f75e549b57b78f90a26a532f462463e3ea88e1a
Anton Bannykh <anton.bannykh@jetbrains.com>
2020-08-24 20:04:51 +0300

    [JS IR] add a flag which forces to load all IR

--- compiler/cli/cli-js/src/org/jetbrains/kotlin/cli/js/K2JsIrCompiler.kt
+++ compiler/cli/cli-js/src/org/jetbrains/kotlin/cli/js/K2JsIrCompiler.kt
@@ -270,6 +270,7 @@ class K2JsIrCompiler : CLICompiler<K2JSCompilerArguments>() {
                 propertyLazyInitialization = arguments.irPropertyLazyInitialization,
                 traceMethods = arguments.traceMethods,
                 focusOnTest = arguments.irFocusOnTest,
+                    forceAllJs = arguments.irForceAllJs,
             )
 
             val jsCode = if (arguments.irDce && !arguments.irDceDriven) compiledModule.dceJsCode!! else compiledModule.jsCode!!
