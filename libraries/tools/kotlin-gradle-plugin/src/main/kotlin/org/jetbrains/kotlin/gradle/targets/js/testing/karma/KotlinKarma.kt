89b4879c1345e17a43939f043f9fd9e8c4f32c65
Alexander Likhachev <Alexander.Likhachev@jetbrains.com>
2021-02-12 14:28:53 +0300

    [Gradle, JS] Postpone TeamCity project property read using 'by lazy'

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/js/testing/karma/KotlinKarma.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/js/testing/karma/KotlinKarma.kt
@@ -59,7 +59,9 @@ class KotlinKarma(
     private var configDirectory: File by property {
         defaultConfigDirectory
     }
-    private val isTeamCity = project.hasProperty(TC_PROJECT_PROPERTY)
+    private val isTeamCity by lazy {
+        project.hasProperty(TC_PROJECT_PROPERTY)
+    }
 
     override val requiredNpmDependencies: Set<RequiredKotlinJsDependency>
         get() = requiredDependencies + webpackConfig.getRequiredDependencies(versions)
