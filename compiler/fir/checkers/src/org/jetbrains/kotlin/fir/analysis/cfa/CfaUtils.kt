fdf259c560abb40a8d6a414d0f207d6674f34a22
Jinseong Jeon <jsjeon@google.com>
2021-02-24 14:48:22 -0800

    FIR: remove unnecessary override in PreliminaryLoopVisitor

--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/cfa/CfaUtils.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/cfa/CfaUtils.kt
@@ -189,10 +189,6 @@ class PropertyInitializationInfoCollector(
                 visitCapturingStatement(simpleFunction, data)
             }
 
-            override fun <F : FirFunction<F>> visitFunction(function: FirFunction<F>, data: FirStatement?) {
-                visitCapturingStatement(function, data)
-            }
-
             override fun visitRegularClass(regularClass: FirRegularClass, data: FirStatement?) {
                 visitCapturingStatement(regularClass, data)
             }
